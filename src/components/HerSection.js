8316c761fe7b52815cea45f9aedf8619abfc4e1a
Rutuja Deshmukh <rutujajd.2001@gmail.com>
2021-06-17 16:36:34 +0530

    first commit

---
+++ src/components/HerSection.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import '../App.css';
+import './HerSection.css';
+
+
+function HerSection() {
+    return ( < div className = 'bg1' >
+        <
+        div className = 'hero-container' >
+        <
+        h2 > Blog Diary < /h2> <
+        p > Take a glance at Blog < /p>  < /
+        div > < /
+        div >
+    );
+}
+
+export default HerSection;
---
+++ src/components/pages/blog.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../../App.css';
+import Cardb from '../Cardb';
+import HerSection from '../HerSection';
+import Footer from '../Footer';
+
+function Home() {
+    return ( <
+        >
+        <
+        HerSection / >
+        <
+        Cardb / >
+        <
+        Footer / >
+        <
+        />
+    );
+}
+
+export default Home;
